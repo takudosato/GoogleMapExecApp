5e0b1e95b991ec4776cad3ba6579bd7d54da4557
takudosato <takudo_sato@sobal.co.jp>
2020-07-13 15:52:48 +0900

    add menu + start GPSActivity

--- app/src/main/java/com/example/googlemapexecapp/GPSActivity.kt
+++ app/src/main/java/com/example/googlemapexecapp/GPSActivity.kt
@@ -2,10 +2,15 @@ package com.example.googlemapexecapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class GPSActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity onCreate", "in")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_g_p_s)
+
+        Log.d("MainActivity onCreate", "out")
     }
 }
--- app/src/main/java/com/example/googlemapexecapp/MainActivity.kt
+++ app/src/main/java/com/example/googlemapexecapp/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.googlemapexecapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity onCreate", "in")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity onCreate", "out")
     }
+
+    /**
+     * onCreateOptionsMenu
+     *
+     * <p>メニュー初期化処理</p>
+     *
+     * @param menu
+     * @return
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        Log.d("MA onCreateOptionsMenu", "in")
+
+        //オプションメニュー用のxmlをインフレイト
+        menuInflater.inflate(R.menu.menu_options_list, menu)
+
+        Log.d("MA onCreateOptionsMenu", "out")
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        Log.d("MA onCreate", "in")
+
+        //メニューで選択された場合
+        when(item.itemId) {
+            //現在地が選択された場合、GPSActivityに遷移する
+            R.id.ofMenuOptionGPS -> {
+                //インテントオブジェクトを生成
+                val intent = Intent(applicationContext, GPSActivity::class.java)
+                //オフジェクトを起動
+                startActivity(intent)
+            }
+
+            //終了が選択された場合、終了処理を行う
+            R.id.ofMenuOptionExit -> {
+                return true
+            }
+        }
+
+        Log.d("MA onCreate", "out")
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
+
+/*
+Log.d("MA onCreate", "in")
+Log.d("MA onCreate", "out")
+*/
