61cb723ca4aba2451c3b0f6f8fb934e8e22a57cd
takudosato <takudo_sato@sobal.co.jp>
2020-07-13 13:39:35 +0900

    add GPSActivity

---
+++ app/src/main/java/com/example/googlemapexecapp/GPSActivity.kt
@@ -0,0 +1,11 @@
+package com.example.googlemapexecapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GPSActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_g_p_s)
+    }
+}
